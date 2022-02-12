daed3292624c8c3f892fa106d3a239226449ccd3
Maciek <msitkowski94@gmail.com>
2022-02-12 21:02:58 +0100

    revert first attempt changes

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -3,43 +3,6 @@ export const SULFURAS = 'Sulfuras, Hand of Ragnaros'
 export const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert'
 export const CONJURED = 'Conjured Mana Cake'
 
-export class GildedRose {
-  items: Array<Item>
-
-  constructor(items = [] as Array<Item>) {
-    this.items = items.map((item) => {
-      const { name, sellIn, quality } = item
-      const args = [name, sellIn, quality] as const
-
-      if (name === SULFURAS) {
-        return new SulfurasItem(...args)
-      }
-
-      if (name === AGED_BRIE) {
-        return new AgedBrieItem(...args)
-      }
-
-      if (name === BACKSTAGE_PASSES) {
-        return new BackstagePassesItem(...args)
-      }
-
-      if (name === CONJURED) {
-        return new ConjuredItem(...args)
-      }
-
-      return item
-    })
-  }
-
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      this.items[i].updateQuality()
-    }
-
-    return this.items
-  }
-}
-
 export class Item {
   name: string
   sellIn: number
@@ -50,77 +13,70 @@ export class Item {
     this.sellIn = sellIn
     this.quality = quality
   }
+}
 
-  updateQuality() {
-    if (this.quality > 0) {
-      this.quality = this.quality - 1
-    }
-    this.sellIn = this.sellIn - 1
-    if (this.sellIn < 0) {
-      if (this.quality > 0) {
-        this.quality = this.quality - 1
-      }
-    }
+export class GildedRose {
+  items: Array<Item>
+
+  constructor(items = [] as Array<Item>) {
+    this.items = items
   }
-}
 
-export class SulfurasItem extends Item {
-  // "Sulfuras" never has to be sold or decreases in quality
-  updateQuality() {}
-}
-
-export class AgedBrieItem extends Item {
   updateQuality() {
-    this.sellIn = this.sellIn - 1
-
-    if (this.quality < 50) {
-      this.quality = this.quality + 1
-    }
-
-    if (this.sellIn < 0) {
-      if (this.quality < 50) {
-        this.quality = this.quality + 1
+    for (let i = 0; i < this.items.length; i++) {
+      if (
+        this.items[i].name != 'Aged Brie' &&
+        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+      ) {
+        if (this.items[i].quality > 0) {
+          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            this.items[i].quality = this.items[i].quality - 1
+          }
+        }
+      } else {
+        if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1
+          if (
+            this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
+          ) {
+            if (this.items[i].sellIn < 11) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1
+              }
+            }
+            if (this.items[i].sellIn < 6) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1
+              }
+            }
+          }
+        }
+      }
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        this.items[i].sellIn = this.items[i].sellIn - 1
+      }
+      if (this.items[i].sellIn < 0) {
+        if (this.items[i].name != 'Aged Brie') {
+          if (
+            this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+          ) {
+            if (this.items[i].quality > 0) {
+              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                this.items[i].quality = this.items[i].quality - 1
+              }
+            }
+          } else {
+            this.items[i].quality =
+              this.items[i].quality - this.items[i].quality
+          }
+        } else {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1
+          }
+        }
       }
     }
-  }
-}
 
-export class BackstagePassesItem extends Item {
-  updateQuality() {
-    this.sellIn = this.sellIn - 1
-
-    if (this.quality < 50) {
-      this.quality = this.quality + 1
-    }
-
-    if (this.sellIn < 10) {
-      if (this.quality < 50) {
-        this.quality = this.quality + 1
-      }
-    }
-
-    if (this.sellIn < 5) {
-      if (this.quality < 50) {
-        this.quality = this.quality + 1
-      }
-    }
-
-    if (this.sellIn < 0) {
-      this.quality = 0
-    }
-  }
-}
-
-export class ConjuredItem extends Item {
-  updateQuality() {
-    if (this.quality > 0) {
-      this.quality = this.quality - 2
-    }
-    this.sellIn = this.sellIn - 1
-    if (this.sellIn < 0) {
-      if (this.quality > 0) {
-        this.quality = this.quality - 2
-      }
-    }
+    return this.items
   }
 }
